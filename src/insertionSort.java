e1b48e30818616820ba821d3b0d812d22be75bf2
Sam Ham <Sam Ham@192.168.0.17>
2020-01-07 19:12:21 -0500

    demonstrates insertion sort

---
+++ src/insertionSort.java
@@ -0,0 +1,31 @@
+public class insertionSort {
+
+	void sort(int arr[]) {
+		int n = arr.length;
+		for (int i = 1; i < n; ++i) {
+			int key = arr[i];
+			int j = i - 1;
+			// move elements greater than key ahead by one
+			while (j >= 0 && arr[j] > key) {
+				arr[j + 1] = arr[j];
+				j = j - 1;
+			}
+			arr[j + 1] = key;
+		}
+	}
+
+	static void printArray(int arr[]) {
+		int n = arr.length;
+		for (int i = 0; i < n; ++i)
+			System.out.print(arr[i] + " ");
+		System.out.println();
+	}
+
+	public static void main(String args[]) {
+		int arr[] = { 5, 4, 6, 8, 7 };
+
+		insertionSort is = new insertionSort();
+		is.sort(arr);
+		printArray(arr);
+	}
+}
