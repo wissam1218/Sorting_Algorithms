7c5413db97a503b8fff8327fd32f25384e352443
Sam Ham <Sam Ham@192.168.0.17>
2020-01-07 19:21:45 -0500

    demonstrates quick sort

--- src/quickSort.java
+++ src/quickSort.java
@@ -36,7 +36,7 @@ public class quickSort {
 		System.out.println();
 	}
 	public static void main(String args[]) {
-		int arr[] = { 10, 7, 8, 9, 1, 5 };
+		int arr[] = {2,3,1,4,7,6,5,4};
 		int n = arr.length;
 		quickSort ob = new quickSort();
 		ob.sort(arr, 0, n - 1);
